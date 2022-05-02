ecf01c1ccc547051af88121b243de97fd70b9057
yinggangli <76646329+yinggangli@users.noreply.github.com>
2022-05-02 22:06:42 +0800

    再次修改NewFile.java

--- TestGit/src/main/java/NewFile.java
+++ TestGit/src/main/java/NewFile.java
@@ -2,5 +2,6 @@ public class NewFile {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
     }
 }
